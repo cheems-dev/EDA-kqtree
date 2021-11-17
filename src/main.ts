4a2ea015005999ac5cd162fee9b1cd0499561ed4
cheems-dev <daprimovaria@gmail.com>
2021-11-16 22:36:22 -0500

    chore: run code with files .csv

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
+import { performance } from "perf_hooks";
 import KDtree from "./class/KD-tree";
 import INode from "./class/Node";
 import convertJSONtoArray from "./functions/convertJSONtoArray";
 
 
 async function main() {
-
-  /* Manejar la asincronia */
-  const points3D = await convertJSONtoArray("./data/test1000.csv");
-  const point: Array<number> = [-0.13324339017031003, -0.28966775580839643, 0.1609404012898191];
-  const point2: Array<number> = [-0.14, 0.236, 0.16804];
-  let root = new INode();
-  const kdtree = new KDtree(3);
-  for (let i = 0; i < points3D.length; i++)
-    root = kdtree.insert(root, points3D[i]);
-  kdtree.search(root, point) ? console.log('Encontrado') : console.log('No encontrado');
-  kdtree.search(root, point2) ? console.log('Encontrado') : console.log('No encontrado');
-
-
+  const file: Array<string> = ['test100', 'test500', 'test1000', 'test5000', 'test10000', 'test20000', 'test100000', 'test200000', 'test350000'];
+  console.log("size, time");
+  for (let i = 0; i < file.length; i++) {
+    const points3D = await convertJSONtoArray(`./data/${file[i]}.csv`);
+    let root = new INode();
+    const kdtree = new KDtree(3);
+    let start: number = performance.now();
+    for (let i = 0; i < points3D.length; i++)
+      root = kdtree.insert(root, points3D[i]);
+    let end: number = performance.now();
+    console.log(`${file[i].slice(4)}, ${+(end - start).toFixed(4)}`);
+  }
 }
 main();
 
